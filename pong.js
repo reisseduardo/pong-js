aecc1f25cd7e3026e14c6f468797724a9f6714bb
Eduardo Reis <reisdasilva.eduardo@gmail.com>
2023-01-24 11:15:16 -0300

    adicionando movimentos basicos

--- pong.js
+++ pong.js
@@ -21,40 +21,76 @@ const line = {
     }
 }
 
+const mouse = {
+    x: 0,
+    y: 0
+}
+
 const leftPaddle = {
     x: 10,
-    y: 240,
+    y: field.h / 2,
     w: line.w,
     h: 200,
+    _move: function () {
+        this.y = mouse.y;
+    },
     draw: function () {
         //desenho da raquete esquerda
         canvasCtx.fillStyle = "#FFFFFF";
         canvasCtx.fillRect(this.x, this.y, this.w, this.h);
+        this._move();
     }
 }
 
 const rightPaddle = {
     x: field.w - line.w - 10,
-    y: 240,
+    y: field.h / 2,
     w: line.w,
     h: 200,
+    _move: function () {
+        this.y = ball.y;
+    },
     draw: function () {
         //desenho da raquete direita
         canvasCtx.fillStyle = "#FFFFFF";
         canvasCtx.fillRect(this.x, this.y, this.w, this.h);
+        this._move();
     }
 }
 
 const ball = {
-    x: 120,
-    y: 240,
+    x: field.w / 2,
+    y: field.h / 2,
     r: 20,
+    speed: 5,
+    yDir: 1,
+    xDir: 1,
+    _calcPosition: function () {
+        if (this.y > field.h || this.y < 0) {
+            this._reverseY();
+        }
+        if (this.x > field.w || this.x < 0) {
+            this._reverseX();
+        }
+    },
+    _reverseX: function () {
+        this.xDir *= -1;
+    },
+    _reverseY: function () {
+        this.yDir *= -1;
+    },
+    _move: function () {
+        this.x += this.xDir * this.speed;
+        this.y += this.yDir * this.speed;
+    },
     draw: function () {
         //desenho da bolinha
         canvasCtx.fillStyle = "#FFFFFF";
         canvasCtx.beginPath();
         canvasCtx.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false);
         canvasCtx.fill();
+        this._move();
+        this._calcPosition();
     }
 
 }
@@ -75,6 +111,28 @@ function draw() {
     ball.draw();
 }
 
-setup();
+window.animateFrame = (function () {
+    return (
+        window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function (callback) {
+            return window.setTimeout(callback, 1000 / 60)
+        }
+    )
+})()
 
-draw();
+function main() {
+    animateFrame(main);
+    draw();
+}
+
+setup();
+main();
+
+canvasEl.addEventListener('mousemove', function (e) {
+    mouse.x = e.pageX;
+    mouse.y = e.pageY;
+})
