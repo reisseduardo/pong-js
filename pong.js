9281deb42e6ba42e66bdf6ac456ded4ecc9ca9ee
Eduardo Reis <reisdasilva.eduardo@gmail.com>
2023-01-27 10:50:59 -0300

    testando código sem usar objetos

--- pong.js
+++ pong.js
@@ -1,186 +1,116 @@
 const canvasEl = document.querySelector("canvas");
 const canvasCtx = canvasEl.getContext("2d");
-//campo
-const field = {
-    w: window.innerWidth,
-    h: window.innerHeight,
-    draw: function () {
-        canvasCtx.fillStyle = "#286047";
-        canvasCtx.fillRect(0, 0, this.w, this.h)
-    }
-}
-//linha central
-const line = {
-    w: 15,
-    h: field.h,
-    draw: function () {
-        canvasCtx.fillStyle = "#FFFFFF";
-        canvasCtx.fillRect(field.w / 2 - this.w / 2, 0, this.w, field.h);
-    }
-}
-//placar do jogo
-const score = {
-    player: 0,
-    computer: 0,
-    increasePlayer: function () {
-        this.player++;
-    },
-    increaseComputer: function () {
-        this.computer++;
-    },
-    draw: function () {
-        canvasCtx.font = "bold 2rem Arial";
-        canvasCtx.textAlign = "center";
-        canvasCtx.textBaseline = "top";
-        canvasCtx.fillStyle = "#FFFFFF";
-        canvasCtx.fillText(this.player, field.w / 4, 30);
-        canvasCtx.fillText(this.computer, field.w / 2 + field.w / 4, 30);
-    }
-}
-const gapX = 10;
-//raquete jogador
-const leftPaddle = {
-    x: gapX,
-    y: field.h / 2 - 100,
-    w: line.w,
-    h: 200,
-    speed: 2,
-    _move: function () {
-        document.addEventListener("keydown", function (e) {
-            if (e.key == "ArrowUp") {
-                leftPaddle.y += leftPaddle.speed * -1;
-            };
-            if (e.key == "ArrowDown") {
-                leftPaddle.y += leftPaddle.speed * 1;
-            };
-            if (leftPaddle.y + leftPaddle.h >= field.h) {
-                leftPaddle.y = field.h - leftPaddle.h;
-            }
-            if (leftPaddle.y <= 0) {
-                leftPaddle.y = 0;
-            }
-        })
-    },
-    draw: function () {
-        canvasCtx.fillStyle = "#FFFFFF";
-        canvasCtx.fillRect(this.x, this.y, this.w, this.h);
-        this._move();
-    }
-}
-//raquete oponente
-const rightPaddle = {
-    x: field.w - line.w - gapX,
-    y: field.h / 2 - 100,
-    w: line.w,
-    h: 200,
-    speed: 1,
-    _move: function () {
-        if (this.y + this.h / 2 < ball.y + ball.r) {
-            this.y += this.speed;
-        } else {
-            this.y -= this.speed;
-        }
-    },
-    speedUp: function () {
-        this.speed = this.speed + 0.5;
-    },
-    speedDown: function () {
-        this.speed = this.speed - 0.5;
-    },
-    draw: function () {
-        canvasCtx.fillStyle = "#FFFFFF";
-        canvasCtx.fillRect(this.x, this.y, this.w, this.h);
-        this._move();
-    }
-}
-//bolinha
-const ball = {
-    x: field.w / 2,
-    y: field.h / 2,
-    r: 20,
-    speed: 10,
-    yDir: 1,
-    xDir: 1,
-    _calcPosition: function () {
-        if (this.x > field.w - this.r - rightPaddle.w - gapX) {
-            if (this.y + this.r > rightPaddle.y && this.y - this.r < rightPaddle.y + rightPaddle.h) {
-                this.xDir *= -1; //colisão raquete oponente
-            } else {
-                score.increasePlayer();//ponto
-                rightPaddle.speedUp();
-                this.reset();
-            }
-        }
-        if (this.x < this.r + leftPaddle.w + gapX) {
-            if (this.y + this.r > leftPaddle.y && this.y - this.r < leftPaddle.y + leftPaddle.h) {
-                this.xDir *= -1; //colisão raquete jogador
-            } else {
-                score.increaseComputer();//ponto
-                rightPaddle.speedDown();
-                this.reset();
-            }
-        }
-        if (this.y > field.h - this.r || this.y < this.r) {
-            this.yDir *= -1; //colisão borda inferior e superior do campo
-        }
-    },
-    _move: function () {
-        this.x += this.xDir * this.speed;
-        this.y += this.yDir * this.speed;
-    },
-    draw: function () {
-        canvasCtx.fillStyle = "#FFFFFF";
-        canvasCtx.beginPath();
-        canvasCtx.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false);
-        canvasCtx.fill();
-        this._move();
-        this._calcPosition();
-    },
-    reset: function () {
-        this.x = field.w / 2;
-        this.y = field.h / 2;
-    }
-}
 //setup
 function setup() {
     canvasEl.width = canvasCtx.width = window.innerWidth;
     canvasEl.height = canvasCtx.height = window.innerHeight;
 }
+let gameSpeed = 0.5;
+//placar
+let playerScore = 0;
+let computerScore = 0;
+//campo
+let fieldW = window.innerWidth;
+let fieldH = window.innerHeight;
+let lineW = 15;
+let gapX = 10;
+//bola
+let ballR = 15;
+let ballX = fieldW / 2;
+let ballY = fieldH / 2;
+let dirX = 1;
+let dirY = 0;
+let ballSpeed = 3;
+//raquete do jogador
+let leftPaddleW = lineW;
+let leftPaddleH = 200;
+let leftPaddleX = gapX;
+let leftPaddleY = fieldH / 2 - leftPaddleH / 2
+//raquete do computador
+let rightPaddleW = lineW;
+let rightPaddleH = 200;
+let rightPaddleX = fieldW - gapX - rightPaddleW;
+let rightPaddleY = fieldH / 2 - rightPaddleH / 2
 //desenhos
 function draw() {
-    //campo
-    field.draw();
-    line.draw();
-    score.draw();
-    //raquetes
-    leftPaddle.draw();
-    rightPaddle.draw();
-    //bolinha
-    ball.draw();
+    fieldDraw();
+    scoreDraw();
+    lineDraw();
+    ballDraw();
+    leftPaddleDraw();
+    rightPaddleDraw();
 }
-//animação
-window.animateFrame = (function () {
-    return (
-        window.requestAnimationFrame ||
-        window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame ||
-        window.oRequestAnimationFrame ||
-        window.msRequestAnimationFrame ||
-        function (callback) {
-            return window.setTimeout(callback, 1000 / 60)
-        }
-    )
-})()
-//função principal a ser chamada no carregamento
-function main() {
-    animateFrame(main);
+function scoreDraw() {
+    canvasCtx.font = "bold 2rem Arial";
+    canvasCtx.textAlign = "center";
+    canvasCtx.textBaseline = "top";
+    canvasCtx.fillStyle = "#FEFAE0";
+    canvasCtx.fillText(playerScore, fieldW / 4, 30);
+    canvasCtx.fillText(computerScore, fieldW / 2 + fieldW / 4, 30);
+}
+function fieldDraw() {
+    canvasCtx.fillStyle = "#283618";
+    canvasCtx.fillRect(0, 0, fieldW, fieldH);
+}
+function lineDraw() {
+    canvasCtx.fillStyle = "#FEFAE0";
+    canvasCtx.fillRect(fieldW / 2 - lineW / 2, 0, lineW, fieldH);
+}
+function ballDraw() {
+    canvasCtx.fillStyle = "#FEFAE0";
+    canvasCtx.beginPath();
+    canvasCtx.arc(ballX, ballY, ballR, 0, 2 * Math.PI, false);
+    canvasCtx.fill();
+}
+function leftPaddleDraw() {
+    canvasCtx.fillStyle = "#BC6C25";
+    canvasCtx.fillRect(leftPaddleX, leftPaddleY, leftPaddleW, leftPaddleH);
+}
+function rightPaddleDraw() {
+    canvasCtx.fillStyle = "#BC6C25";
+    canvasCtx.fillRect(rightPaddleX, rightPaddleY, rightPaddleW, rightPaddleH);
+}
+//funções do jogo
+function game() {
     draw();
+    ballMove();
+    ballCollision();
 }
-//chamadas
+function ballMove() {
+    ballX += dirX * ballSpeed;
+    ballY += dirY * ballSpeed;
+}
+function ballCollision() {
+    if (ballX > fieldW - ballR - rightPaddleW - gapX) {
+        if (ballY + ballR > rightPaddleY && ballY - ballR < rightPaddleY + rightPaddleH) {
+            dirX *= -1; //colisão raquete oponente
+        } else {
+            increasePlayer();//ponto            
+            ballReset();
+        }
+    }
+    if (ballX < ballR + leftPaddleW + gapX) {
+        if (ballY + ballR > leftPaddleY && ballY - ballR < leftPaddleY + leftPaddleH) {
+            dirX *= -1; //colisão raquete jogador
+        } else {
+            increaseComputer();//ponto            
+            ballReset();
+        }
+    }
+    if (ballY > fieldH - ballR || ballY < ballR) {
+        dirY *= -1; //colisão borda inferior e superior do campo
+    }
+}
+function ballReset() {
+    ballX = fieldW / 2;
+    ballY = fieldH / 2;
+}
+function increasePlayer() {
+    playerScore++;
+}
+function increaseComputer() {
+    computerScore++;
+}
+//CHAMADAS TEMP
 setup();
-main();
-//temp
-// canvasEl.addEventListener('mousemove', function (e) {
-//     mouse.x = e.pageX;
-//     mouse.y = e.pageY;
-// })
+setInterval(game, gameSpeed);
